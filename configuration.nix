1145c9050342c73ed6bf84b54c142e23fd9349df
stks56 <t.suenaga428@gmail.com>
2026-04-11 05:43:35 +0000

    default features

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,8 @@
   ];
   nixpkgs.config.allowUnfree = true;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
